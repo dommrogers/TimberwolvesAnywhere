3a62cebc7bc4ef29f78adfba788306fc7f536813
ds5678 <jrpritts7@gmail.com>
2021-03-25 22:17:09 -0400

    Add project files.

---
+++ src/Patches.cs
@@ -0,0 +1,89 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TimberwolvesAnywhere
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(SpawnRegion), "Deserialize")]
+        internal class OnlyTimberwolvesDeserialize
+        {
+            private static void Postfix(SpawnRegion __instance)
+            {
+                if (__instance.m_AiSubTypeSpawned == AiSubType.Wolf)
+                {
+                    //MelonLoader.MelonLogger.Log("Deserialize");
+                    AdjustToRegionSetting(__instance);
+                }
+            }
+        }
+        [HarmonyPatch(typeof(SpawnRegion), "Start")]
+        internal class OnlyTimberwolvesStart
+        {
+            private static void Postfix(SpawnRegion __instance)
+            {
+                if (__instance.m_AiSubTypeSpawned == AiSubType.Wolf)
+                {
+                    //MelonLoader.MelonLogger.Log("Start");
+                    AdjustToRegionSetting(__instance);
+                }
+            }
+        }
+        private static void AdjustToRegionSetting(SpawnRegion spawnRegion)
+        {
+            string sceneName = GameManager.m_ActiveScene;
+            if (sceneName is null) return;
+            if (sceneName == "AshCanyonRegion") SetWolfType(spawnRegion, Settings.options.ashCanyonWolves);
+            else if (sceneName == "CanneryRegion") SetWolfType(spawnRegion, Settings.options.bleakInletWolves);
+            else if (sceneName == "TracksRegion") SetWolfType(spawnRegion, Settings.options.brokenRailroadWolves);
+            else if (sceneName == "CoastalRegion") SetWolfType(spawnRegion, Settings.options.coastalHighwayWolves);
+            else if (sceneName == "WhalingStationRegion") SetWolfType(spawnRegion, Settings.options.desolationPointWolves);
+            else if (sceneName == "MarshRegion") SetWolfType(spawnRegion, Settings.options.forlournMuskegWolves);
+            else if (sceneName == "RiverValleyRegion") SetWolfType(spawnRegion, Settings.options.hushedRiverValleyWolves);
+            else if (sceneName == "MountainTownRegion") SetWolfType(spawnRegion, Settings.options.mountainTownWolves);
+            else if (sceneName == "LakeRegion") SetWolfType(spawnRegion, Settings.options.mysteryLakeWolves);
+            else if (sceneName == "RuralRegion") SetWolfType(spawnRegion, Settings.options.pleasantValleyWolves);
+            else if (sceneName == "CrashMountainRegion") SetWolfType(spawnRegion, Settings.options.timberwolfMountainWolves);
+        }
+        private static void SetWolfType(SpawnRegion spawnRegion,SpawnType spawnType)
+        {
+            if (spawnRegion is null) return;
+            switch (spawnType)
+            {
+                case SpawnType.RegularWolves:
+                    MakeRegularWolves(spawnRegion);
+                    return;
+                case SpawnType.Timberwolves:
+                    MakeTimberwolves(spawnRegion);
+                    return;
+            }
+        }
+        private static void MakeTimberwolves(SpawnRegion spawnRegion)
+        {
+            if (spawnRegion is null) return;
+            GameObject timberwolf = Resources.Load("WILDLIFE_Wolf_grey")?.Cast<GameObject>();
+            GameObject timberwolf_aurora = Resources.Load("WILDLIFE_Wolf_grey_aurora")?.Cast<GameObject>();
+            if (timberwolf && timberwolf_aurora)
+            {
+                spawnRegion.m_AuroraSpawnablePrefab = timberwolf_aurora;
+                spawnRegion.m_SpawnablePrefab = timberwolf;
+            }
+        }
+        private static void MakeRegularWolves(SpawnRegion spawnRegion)
+        {
+            if (spawnRegion is null) return;
+            GameObject regularWolf = Resources.Load("WILDLIFE_Wolf")?.Cast<GameObject>();
+            GameObject regularWolf_aurora = Resources.Load("WILDLIFE_Wolf_aurora")?.Cast<GameObject>();
+            if (regularWolf && regularWolf_aurora)
+            {
+                spawnRegion.m_AuroraSpawnablePrefab = regularWolf_aurora;
+                spawnRegion.m_SpawnablePrefab = regularWolf;
+            }
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModSettings;
+
+namespace TimberwolvesAnywhere
+{
+    public enum SpawnType
+    {
+        Default,
+        Timberwolves,
+        RegularWolves
+    }
+    internal class TimberwolfSettings : JsonModSettings
+    {
+        [Name("Ash Canyon")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType ashCanyonWolves = SpawnType.Default;
+
+        [Name("Bleak Inlet")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType bleakInletWolves = SpawnType.Default;
+
+        [Name("Broken Railroad")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType brokenRailroadWolves = SpawnType.Default;
+
+        [Name("Coastal Highway")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType coastalHighwayWolves = SpawnType.Default;
+
+        [Name("Desolation Point")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType desolationPointWolves = SpawnType.Default;
+
+        [Name("Forlourn Muskeg")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType forlournMuskegWolves = SpawnType.Default;
+
+        [Name("Hushed River Valley")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType hushedRiverValleyWolves = SpawnType.Default;
+
+        [Name("Mountain Town")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType mountainTownWolves = SpawnType.Default;
+
+        [Name("Mystery Lake")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType mysteryLakeWolves = SpawnType.Default;
+
+        [Name("Pleasant Valley")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType pleasantValleyWolves = SpawnType.Default;
+
+        [Name("Timberwolf Mountain")]
+        [Description("The type of wolf to spawn in this region.")]
+        [Choice("Default", "Timberwolves", "Only Regular Wolves")]
+        public SpawnType timberwolfMountainWolves = SpawnType.Default;
+    }
+    internal static class Settings
+    {
+        internal static TimberwolfSettings options;
+        internal static void OnLoad()
+        {
+            options = new TimberwolfSettings();
+            options.AddToModSettings("Timberwolves Anywhere", MenuType.MainMenuOnly);
+        }
+    }
+}
